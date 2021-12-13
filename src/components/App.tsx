8b6c1e24c0ff7528ef9277b408a442235908a3db
phasi <koumiyom4@gmail,com>
2021-12-14 08:31:00 +0900

    firestore導入

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,14 @@
+// Initialize Cloud Firestore through Firebase
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+import { firebaseConfig, projectId } from '../../api';
+const firebaseApp = initializeApp({
+  apiKey: firebaseConfig.apiKey,
+  authDomain: firebaseConfig.authDomain,
+  projectId: projectId
+});
+
+const db = getFirestore();
 export const App = () => {
     return <h1>ディスアプリ</h1>
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { render } from 'react-dom';
 import { App } from './components/App';
-
+import { firebaseConfig, app } from '../api';
+app;
 const rootElement = document.getElementById("root");
 render(<App/>, rootElement)
